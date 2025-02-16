f9fac84611680cd12348ee1bfccd881d28df1e12
Sanchita Jedhe <sanchitajedhe@gmail.com>
2025-02-16 21:00:39 +0530

    update

--- zidio-project/src/App.jsx
+++ zidio-project/src/App.jsx
@@ -5,7 +5,6 @@ import LoginCard from './frontend/LoginCard';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignupPage from './frontend/SignupPage';
-import TaskCard from './components/TaskCard.jsx';
 import CustomNavbar from './components/Navbar.jsx';
 
 
@@ -13,14 +12,14 @@ function MainApp() {
     return (
      
       <Router>
-        
+        <CustomNavbar/>
           <Routes>
             <Route path="/" element={<LoginCard></LoginCard>}></Route>
             <Route path="/home" element={<HomePage />}></Route>
             <Route path="/signup" element={<SignupPage />}></Route>
           </Routes>
-          <CustomNavbar/>
-          <TaskCard/>
+          
+          
         </Router>
       
     );
